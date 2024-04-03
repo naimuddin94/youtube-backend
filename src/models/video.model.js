bb9515a89b08f1b5f0da61b0ff1b0601db606b9b
naimuddin94 <developernaimuddin@gmail.com>
2024-04-03 15:11:47 +0600

    create video model

---
+++ src/models/video.model.js
@@ -0,0 +1,55 @@
+/*
+ * Title: Youtube Application
+ * Description: A Backend Application With Express JS
+ * Author: Md Naim Uddin
+ * Date: 03/04/2024
+ *
+ */
+
+// dependencies
+import mongoose from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
+
+const videoSchema = new mongoose.Schema(
+    {
+        videoFile: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        thumbnail: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        isPublished: {
+            type: Boolean,
+            default: true,
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model('Video', videoSchema);
